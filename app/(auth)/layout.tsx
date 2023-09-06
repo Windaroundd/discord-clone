6c958468e04b7916045098fdee3225318062411f
Phong <phonghqm@gmail.com>
2023-09-07 01:41:12 +0700

    Light & dark theme using shadcn.ui

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex h-full items-center justify-center">{children}</div>
   );
 };
 
-export default Layout;
+export default AuthLayout;
